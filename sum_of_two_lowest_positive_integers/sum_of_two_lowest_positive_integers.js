ab02be347611193a03e4407cb4100f1461aa810e
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-07 23:53:53 +0300

    feat: add task sum of two lowest positive integers

--- sum_of_two_lowest_positive_integers/sum_of_two_lowest_positive_integers.js
+++ sum_of_two_lowest_positive_integers/sum_of_two_lowest_positive_integers.js
@@ -16,3 +16,33 @@ console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), "Sum should be 6");
 console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), "Sum should be 10");
 console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), "Sum should be 24");
 console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), "Sum should be 16");
+
+function sumTwoSmallestNumbers_2(numbers) {
+    let [ a, b ] = numbers.sort((a, b) => a - b)
+    return a + b
+}
+
+console.log(sumTwoSmallestNumbers_2([5, 8, 12, 19, 22]), "Sum should be 13");
+console.log(sumTwoSmallestNumbers_2([15, 28, 4, 2, 43]), "Sum should be 6");
+console.log(sumTwoSmallestNumbers_2([3, 87, 45, 12, 7]), "Sum should be 10");
+console.log(sumTwoSmallestNumbers_2([23, 71, 33, 82, 1]), "Sum should be 24");
+console.log(sumTwoSmallestNumbers_2([52, 76, 14, 12, 4]), "Sum should be 16");
+
+const sumTwoSmallestNumbers_3 = numbers => numbers.sort((x, y) => x - y).slice(0, 2).reduce((x, y) => x + y);
+
+console.log(sumTwoSmallestNumbers_3([5, 8, 12, 19, 22]), "Sum should be 13");
+console.log(sumTwoSmallestNumbers_3([15, 28, 4, 2, 43]), "Sum should be 6");
+console.log(sumTwoSmallestNumbers_3([3, 87, 45, 12, 7]), "Sum should be 10");
+console.log(sumTwoSmallestNumbers_3([23, 71, 33, 82, 1]), "Sum should be 24");
+console.log(sumTwoSmallestNumbers_3([52, 76, 14, 12, 4]), "Sum should be 16");
+
+function sumTwoSmallestNumbers_4(numbers) {
+    const [a, b] = [...numbers].filter(e => e>= 0).sort((a,b) => a - b);
+    return a + b;
+}
+
+console.log(sumTwoSmallestNumbers_4([5, 8, 12, 19, 22]), "Sum should be 13");
+console.log(sumTwoSmallestNumbers_4([15, 28, 4, 2, 43]), "Sum should be 6");
+console.log(sumTwoSmallestNumbers_4([3, 87, 45, 12, 7]), "Sum should be 10");
+console.log(sumTwoSmallestNumbers_4([23, 71, 33, 82, 1]), "Sum should be 24");
+console.log(sumTwoSmallestNumbers_4([52, 76, 14, 12, 4]), "Sum should be 16");
