6ab6d823a4d584e5e493fb4e7693beb66f889485
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-08 16:04:29 +0300

    feat: add task stop gninnips my sdrow

--- stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.js
+++ stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.js
@@ -22,3 +22,32 @@ console.log(spinWords("This is another test"), "/ This is rehtona test");
 console.log(spinWords("You are almost to the last test"), "/ You are tsomla to the last test");
 console.log(spinWords("Just kidding there is still one more"), "/ Just gniddik ereht is llits one more");
 console.log(spinWords("Seriously this is the last one"), "/ ylsuoireS this is the last one");
+
+function spinWords_2(string){
+    return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
+
+console.log(spinWords_2("Welcome"), "/ emocleW");
+console.log(spinWords_2("Hey fellow warriors"), "/ Hey wollef sroirraw");
+console.log(spinWords_2("This is a test"), "/ This is a test");
+console.log(spinWords_2("This is another test"), "/ This is rehtona test");
+console.log(spinWords_2("You are almost to the last test"), "/ You are tsomla to the last test");
+console.log(spinWords_2("Just kidding there is still one more"), "/ Just gniddik ereht is llits one more");
+console.log(spinWords_2("Seriously this is the last one"), "/ ylsuoireS this is the last one");
+
+function spinWords_3(str) {
+    let strArr = str.split(' ');
+    for (let i = 0; i < strArr.length; i++) {
+        if (strArr[i].length >= 5)
+            strArr[i] = strArr[i].split('').reverse().join('');
+    }
+    return strArr.join(' ');
+}
+
+console.log(spinWords_3("Welcome"), "/ emocleW");
+console.log(spinWords_3("Hey fellow warriors"), "/ Hey wollef sroirraw");
+console.log(spinWords_3("This is a test"), "/ This is a test");
+console.log(spinWords_3("This is another test"), "/ This is rehtona test");
+console.log(spinWords_3("You are almost to the last test"), "/ You are tsomla to the last test");
+console.log(spinWords_3("Just kidding there is still one more"), "/ Just gniddik ereht is llits one more");
+console.log(spinWords_3("Seriously this is the last one"), "/ ylsuoireS this is the last one");
