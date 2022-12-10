40c04511ad546c242435038680030526e7664a72
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-10 16:56:43 +0300

    feat: add task rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -24,3 +24,31 @@ console.log("test", "/ grfg", rot13("test"));
 console.log("Test", "/ Grfg", rot13("Test"));
 console.log("az", "/ ", rot13("az"));
 console.log("AZ", "/ ", rot13("AZ"));
+
+
+const rot13_2 = str => str.replace(/[a-z]/gi, letter =>
+    String.fromCharCode(letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13: -13)));
+
+
+console.log("test", "/ grfg", rot13_2("test"));
+console.log("Test", "/ Grfg", rot13_2("Test"));
+
+
+function rot13_3(message){
+    const codeA = 'A'.charCodeAt(0),
+        codeZ = 'Z'.charCodeAt(0),
+        codea = 'a'.charCodeAt(0),
+        codez = 'z'.charCodeAt(0);
+    return message.split('').map(function(char) {
+        let code = char.charCodeAt(0);
+        if(codeA <= code && code <= codeZ){
+            return String.fromCharCode(((code - codeA) + 13) % 26 + codeA);
+        } else if(codea <= code && code <= codez){
+            return String.fromCharCode(((code - codea) + 13) % 26 + codea);
+        }
+        return char;
+    }).join('');
+}
+
+console.log("test", "/ grfg", rot13_3("test"));
+console.log("Test", "/ Grfg", rot13_3("Test"));
