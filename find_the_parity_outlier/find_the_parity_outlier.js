65cea4728fde22846f01e9846907eab4ff692b54
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-10 17:33:17 +0300

    feat: add task find the parity outlier

---
+++ find_the_parity_outlier/find_the_parity_outlier.js
@@ -0,0 +1,44 @@
+/*You are given an array (which will have a length of at least 3,
+but could be very large) containing integers.
+The array is either entirely comprised of odd integers or
+entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+*/
+
+function findOutlier(integers){
+const evenNum = [], oddNum = [];
+for (let num of integers) {
+    if (num === 0 || num % 2 === 0) {
+        evenNum.push(num);
+    } else {
+        oddNum.push(num);
+    }
+}
+return evenNum.length === 1 ? evenNum[0] : oddNum[0];
+}
+
+console.log(findOutlier([0, 1, 2]), "1");
+console.log(findOutlier([1, 2, 3]), "2");
+console.log(findOutlier([2,6,8,10,3]), "3");
+console.log(findOutlier([0,0,3,0,0]), "3");
+console.log(findOutlier([1,1,0,1,1]), "0");
+
+
+function findOutlier_2(integers){
+    const evenNum = [], oddNum = [];
+    integers.forEach((num) => (num === 0 || num % 2 === 0) ? evenNum.push(num) : oddNum.push(num))
+    return evenNum.length === 1 ? evenNum[0] : oddNum[0];
+}
+
+console.log(findOutlier_2([0, 1, 2]), "1");
+console.log(findOutlier_2([1, 2, 3]), "2");
+console.log(findOutlier_2([2,6,8,10,3]), "3");
+console.log(findOutlier_2([0,0,3,0,0]), "3");
+console.log(findOutlier_2([1,1,0,1,1]), "0");
