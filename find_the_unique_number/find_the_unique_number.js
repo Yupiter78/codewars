30e2e901930e5c3b00271fce8dc1fd6063e697fc
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-11 22:00:35 +0300

    feat: add task find the unique number

--- find_the_unique_number/find_the_unique_number.js
+++ find_the_unique_number/find_the_unique_number.js
@@ -19,3 +19,74 @@ console.log(findUniq([ 0, 0, 1 ]), "/ 1");
 console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
 console.log(findUniq([ 1, 1, 2, 1, 1 ]), "/ 2");
 console.log(findUniq([ 3, 10, 3, 3, 3 ]), "/ 10");
+
+
+function findUniq_2(arr) {
+    arr.sort((a,b) => a-b);
+    return arr[0] === arr[1] ? arr.pop() : arr[0];
+}
+
+console.log(findUniq_2([ 1, 0, 0 ]), "/ 1");
+console.log(findUniq_2([ 0, 1, 0 ]), "/ 1");
+console.log(findUniq_2([ 0, 0, 1 ]), "/ 1");
+console.log(findUniq_2([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_2([ 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_2([ 3, 10, 3, 3, 3 ]), "/ 10");
+
+function findUniq_3(arr) {
+    return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
+
+console.log(findUniq_3([ 1, 0, 0 ]), "/ 1");
+console.log(findUniq_3([ 0, 1, 0 ]), "/ 1");
+console.log(findUniq_3([ 0, 0, 1 ]), "/ 1");
+console.log(findUniq_3([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_3([ 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_3([ 3, 10, 3, 3, 3 ]), "/ 10");
+
+function findUniq_4(arr) {
+    let [a, b, c] = arr.slice(0,3);
+    if( a !== b && a !== c ) return a;
+    for( let x of arr ) if( x !== a ) return x
+}
+
+console.log(findUniq_4([ 1, 0, 0 ]), "/ 1");
+console.log(findUniq_4([ 0, 1, 0 ]), "/ 1");
+console.log(findUniq_4([ 0, 0, 1 ]), "/ 1");
+console.log(findUniq_4([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_4([ 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_4([ 3, 10, 3, 3, 3 ]), "/ 10");
+
+function findUniq_5(arr) {
+    return arr.filter(function(elem){
+        return arr.indexOf(elem)===arr.lastIndexOf(elem)
+    })[0]
+}
+
+console.log(findUniq_5([ 1, 0, 0 ]), "/ 1");
+console.log(findUniq_5([ 0, 1, 0 ]), "/ 1");
+console.log(findUniq_5([ 0, 0, 1 ]), "/ 1");
+console.log(findUniq_5([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_5([ 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_5([ 3, 10, 3, 3, 3 ]), "/ 10");
+
+function findUniq_6(arr) {
+    let uniq = {},
+        result;
+    arr.forEach(function(item) {
+        uniq[item] = uniq[item] + 1 || 1;
+    });
+    Object.keys(uniq).forEach(function(key) {
+        if (uniq[key] === 1) {
+            result = key;
+        }
+    });
+    return parseFloat(result);
+}
+
+console.log(findUniq_6([ 1, 0, 0 ]), "/ 1");
+console.log(findUniq_6([ 0, 1, 0 ]), "/ 1");
+console.log(findUniq_6([ 0, 0, 1 ]), "/ 1");
+console.log(findUniq_6([ 1, 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_6([ 1, 1, 2, 1, 1 ]), "/ 2");
+console.log(findUniq_6([ 3, 10, 3, 3, 3 ]), "/ 10");
