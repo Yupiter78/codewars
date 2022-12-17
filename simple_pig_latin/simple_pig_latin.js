21841098f5d50b6abf6e5dafdfb2c12b389238dd
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-17 23:47:10 +0300

    feat: add task simple pig latin

--- simple_pig_latin/simple_pig_latin.js
+++ simple_pig_latin/simple_pig_latin.js
@@ -7,10 +7,19 @@ pigIt('Hello world !');     // elloHay orldway !
 */
 
 function pigIt(str){
+    const marks = [".", ",", "-", "!", "?"];
     return str.split(" ")
-        .map((word, i) => word.slice(1) + word[0] + "ay")
+        .map((word, i) => marks.includes(word) ?
+            word : word.slice(1) + word[0] + "ay")
         .join(" ");
 }
-
-console.log(pigIt('Pig latin is cool'),'/ igPay atinlay siay oolcay');
+console.log(pigIt('Pig latin is cool !'),'/ igPay atinlay siay oolcay !');
 console.log(pigIt('This is my string'),'/ hisTay siay ymay tringsay');
+
+
+function pigIt_2(str){
+    return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+}
+
+console.log(pigIt_2('Pig latin is cool !'),'/ igPay atinlay siay oolcay !');
+console.log(pigIt_2('This is my string'),'/ hisTay siay ymay tringsay');
