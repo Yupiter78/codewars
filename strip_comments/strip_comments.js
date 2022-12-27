e1acbf4a40a87a9e96ec9e2e50c13bd571b9e398
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-12-28 02:40:25 +0300

    feat: add task strip comments

---
+++ strip_comments/strip_comments.js
@@ -0,0 +1,47 @@
+/*Complete the solution so that it strips all text that
+follows any of a set of comment markers passed in.
+
+Any whitespace at the end of the line should also be stripped out.
+
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+The code would be called like so:
+
+var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"
+*/
+
+function solution(input, markers) {
+     const arrStart = input.split("\n").reverse();
+     const result = [];
+     for (let i = arrStart.length; i > 0; i--) {
+         let string = arrStart.pop();
+         for (let marker of markers) {
+             if (!string) continue;
+             if (string.includes(marker)) {
+                 result.push(string.slice(0, string.indexOf(marker)).trim());
+                 string = null;
+             }
+         }
+         if (string) {
+             result.push(string.trim());
+             string = null;
+         }
+     }
+    return result.join("\n");
+}
+
+
+console.log(solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]), "/ apples, plums\npears\noranges")
+console.log(solution("Q @b\nu\ne -e f g", ["@", "-"]), "/ Q\nu\ne")
