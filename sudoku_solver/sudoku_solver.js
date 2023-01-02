62a92e1a6e0517028df10ef32544fafec62a4c2c
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-01-02 23:51:15 +0300

    feat: add task sudoku solver

--- sudoku_solver/sudoku_solver.js
+++ sudoku_solver/sudoku_solver.js
@@ -9,7 +9,7 @@ and can be solved with a brute-force approach.
 
 For Sudoku rules, see the Wikipedia article.
 
-var puzzle = [
+let puzzle = [
             [5,3,0,0,7,0,0,0,0],
             [6,0,0,1,9,5,0,0,0],
             [0,9,8,0,0,0,0,6,0],
@@ -135,4 +135,36 @@ let puzzle_2 = [
 
 
     console.log(sudoku(puzzle), "answer:", solution);
+
+function sudoku_2(puzzle) {
+    const valid = (x,y) => {
+        let v = [];
+        for(let i=0; i<3; i++) {
+            for(let j=0; j<3; j++) {
+                v.push(puzzle[x][i*3+j])
+                v.push(puzzle[i*3+j][y])
+                v.push(puzzle[3*Math.floor(x/3)+i][3*Math.floor(y/3)+j])
+            }
+        }
+        return [1,2,3,4,5,6,7,8,9].filter(e => v.indexOf(e) === -1)
+    }
+    const rec = (x,y) => {
+        if(y === 9) {
+            return puzzle
+        } else if (!puzzle[x][y]) {
+            const correct = valid(x,y).some(i => {
+                puzzle[x][y] = i;
+                return rec((x + 1) % 9,y+(x === 9 ? 1 : 0))
+            })
+            if (correct)
+                return puzzle;
+            puzzle[x][y] = 0;
+        } else {
+            return rec((x + 1) % 9,y+(x=== 8 ? 1 : 0))
+        }
+    }
+    return rec(0,0)
+}
+
+console.log(sudoku(puzzle_2), "answer:", solution);
 
