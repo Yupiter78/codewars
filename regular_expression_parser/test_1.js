3820f9fe45f1071cf73a55adbc8d6f7585c060b1
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-17 23:35:04 +0300

    feat: add additional functionality parse regex

--- regular_expression_parser/test_1.js
+++ regular_expression_parser/test_1.js
@@ -28,67 +28,75 @@ class Str {
 }
 
 function parseRegExp(str) {
-    if (!str) return null;
-    if (str.includes("|") && !str.includes("(")) {
-        let index = str.indexOf("|");
-        const left = parseRegExp(str.slice(0, index));
-        const right = parseRegExp(str.slice(index + 1));
-        if (JSON.stringify(left) === JSON.stringify(right)) return null;
-        if (JSON.stringify(left) === "null" || JSON.stringify(right) === "null") return null;
-        return new Or(left, right);
-    }
-    const regexpList = [];
+    let isOr = true;
 
-    while (str.length > 0) {
-        if (str[0] === '(') {
-            let endIndex = findMatchingClosingParenIndex(str);
-            if (!endIndex) return null;
-            let innerStr = str.slice(1, endIndex);
-            regexpList.push(parseRegExp(innerStr));
-            str = str.slice(endIndex + 1);
-        } else if (str[0] === '[') {
-            let endIndex = findMatchingClosingBracketIndex(str);
-            let innerStr = str.slice(1, endIndex);
-            regexpList.push(parseBracketExpression(innerStr));
-            str = str.slice(endIndex + 1);
-        } else if (str[0] === '|') {
-            let left = regexpList.pop();
-            let right = parseRegExp(str.slice(1));
-            regexpList.push(new Or(left, right));
-            break;
-        } else if (str[0] === '*') {
-            let prevRegExp = regexpList.pop();
-            setLastObjectToZeroOrMore(prevRegExp, regexpList);
-            str = str.slice(1);
-        } else if (str[0] === '+') {
-            let prevRegExp = regexpList.pop();
-            let oneOrMoreRegExp = new Str([prevRegExp, new ZeroOrMore(prevRegExp)]);
-            regexpList.push(oneOrMoreRegExp);
-            str = str.slice(1);
-        } else if (str[0] === '?') {
-            let prevRegExp = regexpList.pop();
-            let zeroOrOneRegExp = new Or(prevRegExp, new Normal(''));
-            regexpList.push(zeroOrOneRegExp);
-            str = str.slice(1);
-        } else if (str[0] === '.') {
-            regexpList.push(new Any());
-            str = str.slice(1);
+    function parseRegex(str) {
+        if (!str) return null;
+        if (isOr && str.includes("|") && !str.includes("(")) {
+            let index = str.indexOf("|");
+            const left = parseRegex(str.slice(0, index));
+            isOr = true;
+            const right = parseRegex(str.slice(index + 1));
+            isOr = true;
+            if (JSON.stringify(left) === JSON.stringify(right)) return null;
+            if (JSON.stringify(left) === "null" || JSON.stringify(right) === "null") return null;
+            return new Or(left, right);
+        }
+        const regexpList = [];
+
+        while (str.length > 0) {
+            isOr = null;
+            if (str[0] === '(') {
+                let endIndex = findMatchingClosingParenIndex(str);
+                if (!endIndex) return null;
+                let innerStr = str.slice(1, endIndex);
+                regexpList.push(parseRegex(innerStr));
+                str = str.slice(endIndex + 1);
+            } else if (str[0] === '[') {
+                let endIndex = findMatchingClosingBracketIndex(str);
+                let innerStr = str.slice(1, endIndex);
+                regexpList.push(parseBracketExpression(innerStr));
+                str = str.slice(endIndex + 1);
+            } else if (str[0] === '|') {
+                let left = regexpList.pop();
+                let right = parseRegex(str.slice(1));
+                regexpList.push(new Or(left, right));
+                break;
+            } else if (str[0] === '*') {
+                let prevRegExp = regexpList.pop();
+                setLastObjectToZeroOrMore(prevRegExp, regexpList);
+                str = str.slice(1);
+            } else if (str[0] === '+') {
+                let prevRegExp = regexpList.pop();
+                let oneOrMoreRegExp = new Str([prevRegExp, new ZeroOrMore(prevRegExp)]);
+                regexpList.push(oneOrMoreRegExp);
+                str = str.slice(1);
+            } else if (str[0] === '?') {
+                let prevRegExp = regexpList.pop();
+                let zeroOrOneRegExp = new Or(prevRegExp, new Normal(''));
+                regexpList.push(zeroOrOneRegExp);
+                str = str.slice(1);
+            } else if (str[0] === '.') {
+                regexpList.push(new Any());
+                str = str.slice(1);
+            } else {
+
+                let sequence = parseSequence(str);
+                if (!sequence) return null;
+                let normalObjects = sequence.split('').map(char => new Normal(char));
+                regexpList.push(...normalObjects);
+                str = str.slice(sequence.length);
+            }
+        }
+        if (regexpList.length === 0) {
+            return null;
+        } else if (regexpList.length === 1) {
+            return regexpList[0];
         } else {
-
-            let sequence = parseSequence(str);
-            if (!sequence) return null;
-            let normalObjects = sequence.split('').map(char => new Normal(char));
-            regexpList.push(...normalObjects);
-            str = str.slice(sequence.length);
+            return new Str(regexpList);
         }
     }
-    if (regexpList.length === 0) {
-        return null;
-    } else if (regexpList.length === 1){
-        return regexpList[0];
-    } else {
-        return new Str(regexpList);
-    }
+    return parseRegex(str);
 }
 
 function setLastObjectToZeroOrMore(prevRegExp, regexpList) {
@@ -189,3 +197,5 @@ console.log(parseRegExp("a|a|a"));
 console.log(parseRegExp("a**"));
 console.log(parseRegExp("a("));
 console.log(parseRegExp("*"));
+console.log(parseRegExp("a|(a|a)"), "Or( Normal('a'), Or( Normal('a'), Normal('a') ) ) ");
+console.log(parseRegExp("(a|a)|a"), "Or( Or( Normal('a'), Normal('a') ), Normal('a') ) ");
