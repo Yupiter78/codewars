a4ad317a8cefba5ea8cdbf5e82a5be6c0568a40a
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-06 23:44:00 +0300

    feat: add task make a spiral

--- make_a_spiral/make_a_spiral.js
+++ make_a_spiral/make_a_spiral.js
@@ -27,9 +27,69 @@ Because of the edge-cases for tiny spirals, the size will be at least 5.
 General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.*/
 
 
-function makeSpiral(n) {
-    if (n < 5) return null;
-    const matrix = Array.from({length: n}, () => Array(n).fill(0));
-    console.log("matrix:", matrix);
+function spiralize(n) {
+    const matrix = Array.from({length: n}, () => new Array(n).fill(0));
+
+    let rowStart = 0,
+        rowEnd = n - 1,
+        colStart = 0,
+        colEnd = n - 1,
+        count = 1;
+
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+
+        // top row
+        for (let i = colStart; i <= colEnd; i++) {
+            matrix[rowStart][i] = 1;
+            count++;
+        }
+        rowStart = rowStart + 1;
+
+        if (colStart !== 0) colStart += 1;
+        // right column
+        for (let i = rowStart; i <= rowEnd; i++) {
+
+            matrix[i][colEnd] = 1;
+            count++;
+        }
+        colEnd = colEnd - 1;
+        if (rowStart === rowEnd) break;
+
+        // bottom row
+        for (let i = colEnd; i >= colStart; i--) {
+            matrix[rowEnd][i] = 1;
+            count++;
+        }
+        rowEnd = rowEnd - 1;
+        if (colStart === colEnd) break;
+
+        // left column
+        for (let i = rowEnd; i > rowStart; i--) {
+            matrix[i][colStart] = 1;
+        }
+
+        rowStart++;
+        rowEnd--;
+        colStart++;
+        colEnd--;
+    }
+
+    return matrix;
 }
-makeSpiral(5);
+console.log(spiralize(5)); // [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+
+console.log(spiralize(10));
+
+/*
+ [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]*/
